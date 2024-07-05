85f8e37940d8060b960b80c53ccc1048e1aa70cf
emanuel <emanuel.vera03>
2024-07-05 13:08:05 -0300

    added db connetion and crud

--- Front/App.py
+++ Front/App.py
@@ -1,7 +1,32 @@
-from flask import Flask
+from flask import Flask, flash, redirect, request, url_for
 from flask import render_template
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+
+#Coneccion a la base de datos
+
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'admin'
+app.config['MYSQL_PASSWORD'] = 'admin'
+app.config['MYSQL_DB'] = 'kaffa_db'
+mysql = MySQL(app)
+
+#Settings
+
+app.secret_key = 'mysecretkey'
+
+# Verificar conexión a la base de datos
+@app.route('/test_db')
+def test_db():
+    try:
+        cur = mysql.connection.cursor()
+        cur.execute('SELECT 1')
+        cur.close()
+        return 'Conexión a la base de datos exitosa'
+    except Exception as e:
+        return str(e)
 
 #Renderizado de paginas
 @app.route('/')
@@ -24,23 +49,73 @@ def formResena():
 def findKaffa():
     return render_template('EncuentraKaffa.html')
 
+@app.route('/Admin')
+def Admin():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM Cafeterias')
+    data = cur.fetchall()
+    return render_template('Admin.html', cafeterias = data)
+
 
 
 #Endpoints
 
-@app.route('/add_cafe')
+@app.route('/add_cafe', methods=['POST'])
 def add_cafe():
-    return "Esto deberia agregar una cafeteria"
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        direccion = request.form['direccion']
+        puntuacion = request.form['puntuacion']
+        imagen = request.form['imagen']
+        detalles = request.form['detalles']
+        cur = mysql.connection.cursor()
+        cur.execute('INSERT INTO Cafeterias  (nombre, direccion, puntuacion, imagen, detalles) VALUES (%s, %s, %s, %s, %s)', 
+        (nombre, direccion, puntuacion, imagen, detalles))
+        mysql.connection.commit()
+        flash('Cafeteria agregada correctamente')
+        return redirect(url_for('Admin'))
 
-@app.route('/edit_cafe')
-def edit_cafe():
-    return "Esto deberia editar una cafeteria"
 
-@app.route('/delete_cafe')
-def delete_cafe():
-    return "Esto deberia eliminar una cafeteria"
+@app.route('/edit/<id>')
+def get_cafe(id):
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM Cafeterias WHERE id = %s',(id))
+    data = cur.fetchall()
+    return render_template('edit-cafe.html', cafeteria = data[0])
+
+@app.route('/update/<id>', methods = ['POST'])
+def update_contact(id):
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        direccion = request.form['direccion']
+        puntuacion = request.form['puntuacion']
+        imagen = request.form['imagen']
+        detalles = request.form['detalles']
+        cur = mysql.connection.cursor()
+        cur.execute("""
+                    UPDATE Cafeterias
+                    SET nombre =  %s,
+                        direccion =  %s,
+                        puntuacion =  %s,
+                        imagen =  %s,
+                        detalles =  %s
+                    WHERE id = %s
+                    """, (nombre, direccion, puntuacion, imagen, detalles, id))
+        mysql.connection.commit()
+        flash('Cafeteria acualizada')
+        return redirect(url_for('Admin'))
+
+
+
+@app.route('/delete/<string:id>')
+def delete_cafe(id):
+    cur = mysql.connection.cursor()
+    cur.execute('DELETE FROM Cafeterias WHERE id = {0}'.format(id))
+    mysql.connection.commit()
+    flash('Cafeteria eliminada correctamente')
+    return redirect(url_for('Admin'))
 
 
 
 if __name__ == '__main__':
-    app.run(port = 5000,debug=True)
+    app.run(port = 3000,debug=True)
